44d422e8218efc92f74ac9a23fe58245fdb2aa0a
EclecticWizard <hodgkinsj95@proton.me>
2024-12-04 19:20:15 +0000

    lists created and sorted

--- 2024/day1.ex
+++ 2024/day1.ex
@@ -3,7 +3,17 @@ defmodule CreateLists do
     case File.read("day1.txt") do
       {:ok, inputs} ->
         normalised = String.replace(inputs, "   ", "\t")
-        IO.inspect(normalised)
+        lines = String.split(normalised, "\n", trim: true)
+        list1 = Enum.map(lines, fn string -> String.split(string, "\t", trim: true)
+          |> List.first()
+        end)
+        list2 = Enum.map(lines, fn string -> String.split(string, "\t", trim: true)
+          |> List.last()
+          end)
+        list1 = Enum.sort(list1)
+        list2 = Enum.sort(list2)
+        IO.inspect(list1)
+        IO.inspect(list2)
       {:error, reason} ->
         IO.puts("Error reading file: #{reason}")
     end
@@ -15,4 +25,5 @@ CreateLists.run
 
 ### Resources
 # https://joyofelixir.com/11-files/
-#
+# https://elixirforum.com/t/looping-through-a-list/45241
+# https://hexdocs.pm/elixir/Enum.html#map/2
