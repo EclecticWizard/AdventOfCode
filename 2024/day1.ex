34fc0808634c9093924441b61acd108cdc82e6cf
EclecticWizard <hodgkinsj95@proton.me>
2024-12-04 19:51:40 +0000

    solved part 1

--- 2024/day1.ex
+++ 2024/day1.ex
@@ -1,4 +1,5 @@
-defmodule CreateLists do
+
+defmodule DayOne do
   def run do
     case File.read("day1.txt") do
       {:ok, inputs} ->
@@ -12,18 +13,30 @@ defmodule CreateLists do
           end)
         list1 = Enum.sort(list1)
         list2 = Enum.sort(list2)
+        list1 = Enum.map(list1, fn string -> String.to_integer(string) end)
+        list2 = Enum.map(list2, fn string -> String.to_integer(string) end)
         IO.inspect(list1)
         IO.inspect(list2)
+
+        distances = Enum.zip(list1, list2)
+          |> Enum.map(fn {location1, location2} -> abs(location1 - location2) end)
+        distance = Enum.sum(distances)
+        IO.inspect(distance)
+
       {:error, reason} ->
         IO.puts("Error reading file: #{reason}")
     end
   end
 end
 
-CreateLists.run
+DayOne.run
 
 
 ### Resources
 # https://joyofelixir.com/11-files/
 # https://elixirforum.com/t/looping-through-a-list/45241
+# https://hexdocs.pm/elixir/Enum.html#sort/1
 # https://hexdocs.pm/elixir/Enum.html#map/2
+# https://hexdocs.pm/elixir/Enum.html#zip/1
+# https://hexdocs.pm/elixir/Kernel.html#abs/1
+# https://hexdocs.pm/elixir/String.html#to_integer/1
